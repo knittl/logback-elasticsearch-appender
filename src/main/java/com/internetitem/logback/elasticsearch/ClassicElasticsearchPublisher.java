f5288bcafaa9a5e5f7227ed784f96c9a41a07922
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2020-01-08 07:26:55 +0100

    WIP Serialize (structured) arguments to Elasticsearch JSON

--- src/main/java/com/internetitem/logback/elasticsearch/ClassicElasticsearchPublisher.java
+++ src/main/java/com/internetitem/logback/elasticsearch/ClassicElasticsearchPublisher.java
@@ -2,10 +2,12 @@ package com.internetitem.logback.elasticsearch;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Objects;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.Context;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.internetitem.logback.elasticsearch.arguments.StructuredArgument;
 import com.internetitem.logback.elasticsearch.config.ElasticsearchProperties;
 import com.internetitem.logback.elasticsearch.config.HttpRequestHeaders;
 import com.internetitem.logback.elasticsearch.config.Property;
@@ -15,9 +17,11 @@ import com.internetitem.logback.elasticsearch.util.ClassicPropertyAndEncoder;
 import com.internetitem.logback.elasticsearch.util.ErrorReporter;
 
 public class ClassicElasticsearchPublisher extends AbstractElasticsearchPublisher<ILoggingEvent> {
+    private final String nonStructuredArgsFieldPrefix;
 
     public ClassicElasticsearchPublisher(Context context, ErrorReporter errorReporter, Settings settings, ElasticsearchProperties properties, HttpRequestHeaders headers) throws IOException {
         super(context, errorReporter, settings, properties, headers);
+        nonStructuredArgsFieldPrefix = Objects.toString(settings.getNonStructuredArgumentsFieldPrefix(), "arg");
     }
 
     @Override
@@ -40,9 +44,39 @@ public class ClassicElasticsearchPublisher extends AbstractElasticsearchPublishe
             gen.writeObjectField("message", formattedMessage);
         }
 
-        if(settings.isIncludeMdc()) {
+        if (settings.isIncludeMdc()) {
             for (Map.Entry<String, String> entry : event.getMDCPropertyMap().entrySet()) {
                 gen.writeObjectField(entry.getKey(), entry.getValue());
+            }
+        }
+
+        serializeArguments(gen, event);
+    }
+
+    private void serializeArguments(JsonGenerator gen, ILoggingEvent event) throws IOException {
+        boolean includeStructuredArguments = settings.isIncludeStructuredArguments();
+        boolean includeNonStructuredArguments = settings.isIncludeNonStructuredArguments();
+        if (!includeStructuredArguments && !includeNonStructuredArguments) {
+            return;
+        }
+
+        Object[] args = event.getArgumentArray();
+
+        if (args == null) {
+            return;
+        }
+
+        for (int argIndex = 0; argIndex < args.length; argIndex++) {
+            Object arg = args[argIndex];
+
+            if (arg instanceof StructuredArgument) {
+                if (includeStructuredArguments) {
+                    StructuredArgument structuredArgument = (StructuredArgument) arg;
+                    structuredArgument.writeTo(gen);
+                }
+            } else if (includeNonStructuredArguments) {
+                String fieldName = nonStructuredArgsFieldPrefix + argIndex;
+                gen.writeObjectField(fieldName, arg);
             }
         }
     }
